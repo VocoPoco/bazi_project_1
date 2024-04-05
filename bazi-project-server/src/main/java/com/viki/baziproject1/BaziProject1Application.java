b7118758cbe7bdb0456e9dfa961ccbb10b85d7b4
VocoPoco <VocoPoco@email.com>
2024-04-05 15:44:57 +0300

    feat: created maven project

---
+++ bazi-project-server/src/main/java/com/viki/baziproject1/BaziProject1Application.java
@@ -0,0 +1,13 @@
+package com.viki.baziproject1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaziProject1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BaziProject1Application.class, args);
+	}
+
+}
---
+++ bazi-project-server/src/test/java/com/viki/baziproject1/BaziProject1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.viki.baziproject1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaziProject1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
